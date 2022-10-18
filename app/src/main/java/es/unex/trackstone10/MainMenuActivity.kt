d5498ffc159ece397a1f921b6b5fd47e003162d0
wadzetma <wadzetma@alumnos.unex.es>
2022-10-18 16:39:17 +0200

    Interfaz del menu principal

--- app/src/main/java/es/unex/trackstone10/MainMenuActivity.kt
+++ app/src/main/java/es/unex/trackstone10/MainMenuActivity.kt
@@ -3,7 +3,7 @@ package es.unex.trackstone10
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
