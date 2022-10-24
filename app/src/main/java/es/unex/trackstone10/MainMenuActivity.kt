21b28991b5723fadab92870c437d59aad8672ea1
wadzetma <wadzetma@alumnos.unex.es>
2022-10-24 18:37:40 +0200

    Button browse cards implemented

--- app/src/main/java/es/unex/trackstone10/MainMenuActivity.kt
+++ app/src/main/java/es/unex/trackstone10/MainMenuActivity.kt
@@ -1,11 +1,21 @@
 package es.unex.trackstone10
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val bBrowse =findViewById<Button>(R.id.browseButton)
+
+        bBrowse.setOnClickListener(){ view ->
+            intent = Intent(this, BrowseCardsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
