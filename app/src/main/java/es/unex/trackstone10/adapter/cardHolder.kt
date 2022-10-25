5f80cf017339bdf4d1530317643f76eb36d28f5e
wadzetma <wadzetma@alumnos.unex.es>
2022-10-25 16:20:30 +0200

    supuestas imagenes añadidas

--- app/src/main/java/es/unex/trackstone10/adapter/cardHolder.kt
+++ app/src/main/java/es/unex/trackstone10/adapter/cardHolder.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
 import es.unex.trackstone10.Cards
 import es.unex.trackstone10.R
 
@@ -21,5 +22,6 @@ class cardHolder(view: View) : ViewHolder(view) {
             4 -> cardCost.text = "400"
             5 -> cardCost.text = "1600"
         }
+        Glide.with(image.context).load(cards.Image).into(image)
     }
 }
