e57f186b26784b7f384ba4a713389546c56bddd8
wadzetma <wadzetma@alumnos.unex.es>
2022-10-25 12:53:14 +0200

    Intents main menu hechos y manifest arreglado

--- app/src/main/java/es/unex/trackstone10/MainMenuActivity.kt
+++ app/src/main/java/es/unex/trackstone10/MainMenuActivity.kt
@@ -12,9 +12,39 @@ class MainMenuActivity : AppCompatActivity() {
 
 
         val bBrowse =findViewById<Button>(R.id.browseButton)
+        val bDecks = findViewById<Button>(R.id.decksButton)
+        val bFavs = findViewById<Button>(R.id.favButton)
+        val bHero = findViewById<Button>(R.id.heroButton)
+        val bProfile = findViewById<Button>(R.id.profileButton)
+        val bCardback = findViewById<Button>(R.id.cardbacksButton)
 
         bBrowse.setOnClickListener(){ view ->
             intent = Intent(this, BrowseCardsActivity::class.java)
+            startActivity(intent)
+        }
+
+        bDecks.setOnClickListener(){view ->
+//            intent = Intent(this, )
+//            startActivity(intent)
+        }
+
+        bFavs.setOnClickListener(){ view ->
+//            intent = Intent(this, )
+//            startActivity(intent)
+        }
+
+        bHero.setOnClickListener(){view ->
+            intent = Intent(this, Heroe_skinInfoActivity::class.java)
+            startActivity(intent)
+        }
+
+        bProfile.setOnClickListener(){ view ->
+            intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        bCardback.setOnClickListener(){view ->
+            intent = Intent(this, Card_backInfoActivity::class.java)
             startActivity(intent)
         }
     }
