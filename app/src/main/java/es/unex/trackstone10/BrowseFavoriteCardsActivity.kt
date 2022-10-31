9aa1bc4546d3be498bd062d079b012e72da9a01c
amejiasv <amejiasv@alumnos.unex.es>
2022-10-31 13:44:35 +0100

    Modified UI 4.0

--- app/src/main/java/es/unex/trackstone10/BrowseFavoriteCardsActivity.kt
+++ app/src/main/java/es/unex/trackstone10/BrowseFavoriteCardsActivity.kt
@@ -3,9 +3,9 @@ package es.unex.trackstone10
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class favoritesMenuActivity : AppCompatActivity() {
+class BrowseFavoriteCardsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_favorites_menu)
+        setContentView(R.layout.activity_browse_favorite_cards)
     }
 }
