ca3148833cf115f06eef03cf081c210fe0176894
amejiasv <amejiasv@alumnos.unex.es>
2022-11-20 22:14:35 +0100

    showCardBackDetails done

--- app/src/main/java/es/unex/trackstone10/Card_backInfoActivity.kt
+++ app/src/main/java/es/unex/trackstone10/Card_backInfoActivity.kt
@@ -2,10 +2,22 @@ package es.unex.trackstone10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import es.unex.trackstone10.API.CardBackResponse
+import es.unex.trackstone10.databinding.ActivityCardBackInfoBinding
 
 class Card_backInfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCardBackInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_card_back_info)
+        binding = ActivityCardBackInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val cardBacks = intent.getSerializableExtra("CARD_BACK_OBJ") as CardBackResponse
+        binding.cardBackDetailsName.text = cardBacks.name
+        binding.cardBackDetail1.text = cardBacks.text
+        Glide.with(binding.cardBackDetails.context).load(cardBacks.image).into(binding.cardBackDetails)
+
     }
 }
