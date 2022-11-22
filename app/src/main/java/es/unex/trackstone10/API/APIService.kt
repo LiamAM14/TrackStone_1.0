7e990c7f7e3099aee86a4082ee146d2453cc56a0
LiamAM14 <wadzetma@alumnos.unex.es>
2022-11-22 01:27:48 +0100

    Añadida llamada para coger cartas de una clase

--- app/src/main/java/es/unex/trackstone10/API/APIService.kt
+++ app/src/main/java/es/unex/trackstone10/API/APIService.kt
@@ -29,6 +29,14 @@ interface APIService {
         @Query("locale") locale: String
     ): Response<CardResponseList>
 
+    @GET("/hearthstone/cards")
+    suspend fun getCardsByClass(
+        @Query("class") HSclass: String,
+        @Query("set") set: String,
+        @Query("sort") byMana: String,
+        @Query("locale") locale: String
+    ): Response<CardResponseList>
+
     @GET("/hearthstone/cardbacks")
     suspend fun getCardBacksByName(
         @Query("locale") locale: String
